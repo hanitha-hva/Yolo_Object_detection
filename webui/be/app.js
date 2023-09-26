500d4db2817b0acd2b6050630a8b528acb06efb1
Arjith_arch <arjtihh7@gmail.com>
2023-09-26 22:10:52 +0530

    setup done

--- webui/be/app.js
+++ webui/be/app.js
@@ -1,18 +1,24 @@
 const exp=require('express')
 const axios=require('axios')
+const path=require('path')
 const app=exp()
+
 const cors=require('cors')
 app.use(cors())
-app.get('/',(req,res)=>{
-    res.send('hello')
-})
+const config = {
+    headers: {
+      'x-api-key': 'dknk339fbbb',
+    },
+  };
+  
 app.get('/api/test',(req,res)=>{
     res.send('gg server')
 })
 app.get('/api/django',(req,res)=>{
-    axios.get('http://127.0.0.1:8000/')
+    axios.get('http://127.0.0.1:8000/',config)
     .then((r)=>res.send(r.data))
 })
+app.use(exp.static(path.join(__dirname, '../fe/dist_prod')));
 app.listen(3000,()=>{
     console.log('serever started')
 })
--- webui/dj_server/ml_server/app/views.py
+++ webui/dj_server/ml_server/app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render,HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 def home(req):
-    return HttpResponse('hello from django')
+
+    apikey='dknk339fbbb'
+    key=req.headers.get('x-api-key')
+    
+    if key==apikey:
+        return HttpResponse('hello from hanitha')
+    else:
+        return HttpResponse(status=500)
+        
